9dbe670ce1f79dff205307f1caab41a95507da01
Antti <antti.pessa@tuni.fi>
2019-12-13 16:41:21 +0000

    management view show and delete working

--- controllers/questionnaire.js
+++ controllers/questionnaire.js
@@ -13,6 +13,17 @@ module.exports = {
 
     async show(request, response) {
         console.log('Management View: Show');
+        try {
+            const game = await Questionnaire.findById(request.params.id)
+                .exec();
+            response.render('game/game', { game });
+        }
+        // If game wasn't found with the given id, redirect back to /questionnaires
+        catch (err) {
+            console.error(err);
+            console.log('Redirecting to /questionnaires');
+            return response.redirect('/questionnaires');
+        }
     },
 
     async create(request, response) {
@@ -33,9 +44,16 @@ module.exports = {
 
     async delete(request, response) {
         console.log('Management View: Delete');
+        const game = await Questionnaire.findById(request.params.id).exec();
+        response.render('partials/exercise_delete', {
+            game
+        });
     },
 
     async processDelete(request, response) {
         console.log('Management View: Process Delete');
+        await Questionnaire.findByIdAndDelete(request.params.id).exec();
+        request.flash('successMessage', 'Questionnaire removed successfully.');
+        response.redirect('/questionnaires');
     },
 }
