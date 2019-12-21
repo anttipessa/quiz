b3d6bc745980544965dc1896c4b3493016dad3ae
Antti <antti.pessa@tuni.fi>
2019-12-21 13:07:41 +0000

    initial management view testing

--- test/assignment/management.test.js
+++ test/assignment/management.test.js
@@ -4,16 +4,71 @@
 require('dotenv').config();
 const config = require('config');
 const http = require('http');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const expect = chai.expect;
+chai.use(chaiHttp);
 
 const app = require('../../app.js');
 const admin = config.get('admin');
+const User = require('../../models/user');
+
+const loginUrl = '/users/login';
+const mview = '/questionnaires';
 
 describe('Game: A+ protocol', function() {
-    it('C: create operation available');
+    let request;
 
-    it('R: read operation available');
 
-    it('U: update operation available');
+    beforeEach(async function() {
+        try {
+            // remove all users from the database and re-create admin user
+            await User.deleteMany({});
 
-    it('D: delete operation available');
+            const userData = { ...admin, role: 'admin' };
+            const user = new User(userData);
+            await user.save();
+        } catch (err) {
+            // eslint-disable-next-line no-console
+            console.log(err);
+            throw err;
+        }
+    });
+    describe('/questionnaires', function() {
+
+        let payload;
+
+        beforeEach(function() {
+            request = chai.request.agent(app);
+            // create a new copy of admin for each test
+            payload = { ...admin };
+            delete payload.name;
+            delete payload.role;
+
+            // admin is logged in before every test
+            request
+                .post(loginUrl)
+                .type('form')
+                .send(payload);
+        });
+
+        afterEach(function() {
+            request.close();
+        });
+
+        it('It should allow admin user to access /questionnaires', async function() {
+            const response = await request
+                .post(mview);
+        });
+
+
+        it('C: create operation available');
+
+
+        it('R: read operation available');
+
+        it('U: update operation available');
+
+        it('D: delete operation available');
+    });
 });
