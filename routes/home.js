e17ef6d5decf228741d287f242a169bca7e116d6
Antti Pessa <anttipessa@gmail.com>
2020-12-12 00:13:05 +0200

    Config deployment/tagging

--- routes/home.js
+++ routes/home.js
@@ -5,5 +5,7 @@ const router = express.Router();
 const HomeController = require('../controllers/home');
 
 router.get('/', HomeController.showExercise);
-
+router.get('/health', (req, res) => {
+    res.send('ok');
+});
 module.exports = router;
